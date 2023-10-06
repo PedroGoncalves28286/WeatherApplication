cec1536ead43cd8423b654116f606889ab97cb88
Pedro Goncalves <pedromfonsecagoncalves@gmail.com>
2023-10-06 20:30:47 +0100

    Init Project

---
+++ WeatherApplication/Data/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApplication.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [MaxLength(15, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Password { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public override string Email { get; set; }
+    }
+}
